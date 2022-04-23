bc9daee1f4efbdae1d9b04d8883acd2bcb99763f
Lionel AREL <lionel.arel@outlook.fr>
2022-04-23 18:31:09 +0200

    searchbar

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -1,9 +1,23 @@
 import React from 'react';
+import search from "./../assets/search.svg";
+import Select from 'react-select'
 
 const SearchBar = () => {
+    // const items = []
+    // for(var i = 1950;i <  2023;i++){
+    //     items.push(<input value={i.toString()} key={i} type="checkbox" />);
+    // }
     return (
         <div>
-            navBar
+            <form action="">
+                <label htmlFor="input">
+                    <div className="input-wrapper">
+                        <input type="text" />
+                        <button className="searchButton">Search</button>
+                        <img className='searchIcon' src={search} alt="" />
+                    </div>
+                </label>
+            </form>
         </div>
     );
 };
