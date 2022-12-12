49556768137d19dcabcea7b94b257fad84b9bf05
Lionel AREL <lionel.arel@outlook.fr>
2022-12-12 12:12:39 +0100

    update front

--- src/components/Movie.tsx
+++ src/components/Movie.tsx
@@ -4,7 +4,10 @@ const Movie:FunctionComponent<any> = ({movie, ...rootDOMAttributes}) => {
     return (
         <div className='movie' {...rootDOMAttributes}>
             <img src={movie.poster_path ? "https://image.tmdb.org/t/p/w500/" + movie.poster_path : ""} alt="" />
-            <h1>Title : {movie.title} <br /> <br /> Date : {movie.release_date} <br /> <br /> Note : {movie.vote_average}  <br /> <br /> adult : {movie.adult.toString()}</h1>
+            <h1>🎥 Title : {movie.title} <br /> <br /> 
+            ⏲ Date : {movie.release_date} <br /> <br /> 
+            ⭐️ Note : {movie.vote_average}  <br /> <br /> 
+            🔞  adult : {movie.adult.toString()}</h1>
         </div>
     );
 };
--- src/pages/Search.tsx
+++ src/pages/Search.tsx
@@ -6,6 +6,7 @@ import FetchData from '../services/fetchData-service';
 import ReactModal from 'react-modal';
 import Modal from 'react-modal';
 import closeLogo from './../assets/close.svg'
+import { AnyNsRecord } from 'dns';
 
 
 const Search = () => {
@@ -54,33 +55,66 @@ const Search = () => {
         setPageCount(e.selected);
     }
 
+    function handleClickMovie(movie:any){
+        console.log(movie)
+        handleOpenModal(movie.id);
+        FetchData.getMovie(movie.id).then((movie) => {
+            console.log("movie",movie)
+            setSelectedMovie(movie);
+        })
+    }
+
     return (
             <div className="container" id='main'>
                 <SearchBar setDetails={setDetails} setMovies={setMovies} setIsDetails={setIsDetails} setTotalPages = {setTotalPages} pageCount={pageCount} isDetails={isDetails} setPageCount = {setPageCount}/>
                 
                 <ReactModal 
-                isOpen={showModal}
-                contentLabel="Movie detail"
-                onRequestClose={handleCloseModal}
-                className="Modal"
-                overlayClassName="Overlay"
-                >
-                <div className='close' onClick={handleCloseModal}><img src={closeLogo} alt="" /></div>
-                <div className='posterContainer'>
-                    <img className='poster' src={selectedMovie?.poster_path ? "https://image.tmdb.org/t/p/w500/" + selectedMovie.poster_path : ""} alt="" />
-                </div>
-                <div className='details'>
-                    <div className='title'>{selectedMovie?.title}</div>
-                    <div className='synopsis'>Synopsis : {selectedMovie?.overview}</div>
-                </div>
-                <div className='youtubeContainer'>
-                    {movieVideo ? <iframe className='youtube' src={"https://www.youtube.com/embed/" + movieVideo} title="YouTube video player" frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowFullScreen></iframe> : ""}
-                </div>
+                    isOpen={showModal}
+                    contentLabel="Movie detail"
+                    onRequestClose={handleCloseModal}
+                    className="Modal"
+                    overlayClassName="Overlay"
+                    >
+                    <div className='close' onClick={handleCloseModal}><img src={closeLogo} alt="" /></div>
+                    <div className='posterContainer'>
+                        <img className='poster' src={selectedMovie?.poster_path ? "https://image.tmdb.org/t/p/w500/" + selectedMovie.poster_path : ""} alt="" />
+                    </div>
+                    <div className='details'>
+                        <div className='title'>{selectedMovie?.title}</div>
+                        <div className='synopsis'>🎬 Synopsis : {selectedMovie?.overview}</div>
+                        {selectedMovie?.budget != 0 ?  <div className='budget'>💰 Budget : {selectedMovie?.budget.toLocaleString('de-DE')}$</div> : null}
+                        {selectedMovie?.genres.lenght != 0 ? 
+                        <div className='genres'>
+                            <div>
+                            📑 Genres :
+                            </div>
+                            {selectedMovie?.genres.map((genre:any) => {
+                                return <div key={genre.id} className='genre'>{genre.name}</div>
+                            })}
+                        </div>
+                        : null }
+                        {selectedMovie?.production_companies.lenght != 0 ? 
+                        <div className='campanies'>
+                            <div>
+                            🏭 Campanies :
+                            </div>
+                            {selectedMovie?.production_companies.map((campanie:any) => {
+                                return <div className='campanie' key={campanie.id}>
+                                    {campanie?.logo_path ? <img src={"https://image.tmdb.org/t/p/w500" + campanie?.logo_path} alt="" /> : null}
+                                        {campanie.name}
+                                    </div>
+                            })}
+                        </div>
+                        : null }
+                    </div>
+                    <div className='youtubeContainer'>
+                        {movieVideo ? <iframe className='youtube' src={"https://www.youtube.com/embed/" + movieVideo} title="YouTube video player" frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowFullScreen></iframe> : ""}
+                    </div>
 
                 </ReactModal>
 
                 <div className='movieContainer'>
-                    {movies?(movies.length != 0?movies.map((movie) => <Movie onClick={() => {handleOpenModal(movie.id);setSelectedMovie(movie)}} setSelectedMovie={setSelectedMovie} handleOpenModal={handleOpenModal} key={movie.id} movie={movie}/>): <div>Aucun resultat</div>): <div>chargement</div>}
+                    {movies?(movies.length != 0?movies.map((movie) => <Movie key={movie.id} onClick={() => handleClickMovie(movie)} movie={movie}/>): <div>Aucun resultat</div>): <div>chargement</div>}
                 </div>
                 <div className="pages">
                     <ReactPaginate
